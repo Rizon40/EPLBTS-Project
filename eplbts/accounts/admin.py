6adba458a8b72fcde21af7dea2ab1dd652be7a1f
Rizon40 <23101140@uap-bd.edu>
2026-03-07 19:09:56 +0600

    Register models in Django admin

--- eplbts/accounts/admin.py
+++ eplbts/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'role', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role', 'phone_number')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- eplbts/core/admin.py
+++ eplbts/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Hospital, HospitalStatus, PatientEvent, TransferRequest, Notification, AuditLog
+
+admin.site.register(Hospital)
+admin.site.register(HospitalStatus)
+admin.site.register(PatientEvent)
+admin.site.register(TransferRequest)
+admin.site.register(Notification)
+admin.site.register(AuditLog)
