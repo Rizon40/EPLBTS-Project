1df80e6a59a8a02f3791c0ec0c07500d1246e197
Abir146 <23101146@uap-bd.edu>
2026-04-04 21:45:39 +0600

    Update accounts app admin.py

--- eplbts/accounts/admin.py
+++ eplbts/accounts/admin.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
+# accounts/admin.py
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'email', 'role', 'is_active']
+    list_display = ['username', 'email', 'role', 'hospital', 'is_active']
     fieldsets = UserAdmin.fieldsets + (
-        ('Role Info', {'fields': ('role', 'phone_number')}),
+        ('Role Info', {'fields': ('role', 'phone_number', 'hospital')}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
