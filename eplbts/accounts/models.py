6865f4e9585e2a3e7b03a3f18df5fe1e47be5d39
Abir146 <23101146@uap-bd.edu>
2026-04-04 21:42:25 +0600

    Update accounts app models.py

--- eplbts/accounts/models.py
+++ eplbts/accounts/models.py
@@ -1,8 +1,3 @@
-from django.db import models
-
-# Create your models here.
-# accounts/models.py
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -10,10 +5,11 @@ from django.db import models
 class CustomUser(AbstractUser):
 
     ROLE_CHOICES = [
-        ('paramedic',   'Paramedic / Ambulance Operator'),
+        ('patient', 'Patient / General User'),
+        ('paramedic',  'Paramedic / Ambulance Operator'),
         ('hospital_admin', 'Hospital Administrator'),
-        ('authority',   'Authority / Monitoring Officer'),
-        ('admin',       'System Administrator'),
+        ('authority',  'Authority / Monitoring Officer'),
+        ('admin', 'System Administrator'),
     ]
 
     role = models.CharField(
@@ -24,10 +20,21 @@ class CustomUser(AbstractUser):
 
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
+    hospital = models.ForeignKey(
+        'core.Hospital',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='admins'
+    )
+
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
 
-    # Helper properties for templates
+    @property
+    def is_patient(self):
+        return self.role == 'patient'
+
     @property
     def is_paramedic(self):
         return self.role == 'paramedic'
